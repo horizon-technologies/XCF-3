9b1951acb8f112dbe4c4f415300a07b1c906e13b
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-02-20 22:56:44 -0500

    Reorganize networking

--- lua/xcf/core/networking/data_vars/data_vars_sh.lua
+++ lua/xcf/core/networking/data_vars/data_vars_sh.lua
@@ -53,80 +53,112 @@ else
 	hook.Add("InitPostEntity", "XCF_DetectHostClient", function()
 		if LocalPlayer():IsListenServerHost() then
 			XCF.IsListenServer = true
+			hook.Remove("InitPostEntity", "XCF_DetectHostClient")
 		end
-		hook.Remove("InitPostEntity", "XCF_DetectHostClient")
 	end)
 end
 print("XCF: Listen server mode is " .. tostring(XCF.IsListenServer))
 
 local XCF_DATA_VAR_LIMIT_EXPONENT = 8 -- Maximum number of data vars allowed as an exponent of 2
 local XCF_DATA_VAR_MAX_MESSAGE_SIZE = 128 -- Maximum size of a data var message in bytes
-local ServerString = "Server" -- Represents the server as if it were a player (technically this should be a player entity, but the server doesn't have one.)
 
 if SERVER then util.AddNetworkString("XCF_DV_NET") end
 
 -- TODO: Add queue for rate limitting per variable (and forcing option)
+local ServerKey = "Server"
 
---- Sets a data variable and networks it to the other realm.
---- Target is either a player (client data), or nil (server data)
-function XCF.SetDataVar(Key, Value, Target)
+-- Internal helper to send a datavar across realms
+local function SendDataVar(DataVar, Value, SyncServerRealm, TargetPlayer)
+	net.Start("XCF_DV_NET")
+	net.WriteUInt(DataVar.UUID, XCF_DATA_VAR_LIMIT_EXPONENT)
+	net.WriteBool(SyncServerRealm)
+	DataVar.Type.Write(Value)
+
+	if SERVER then
+		if TargetPlayer then net.Send(TargetPlayer)
+		else net.Broadcast() end
+	else
+		net.SendToServer()
+	end
+end
+
+--- Synchronizes server data with the other realm
+--- Called from server: Sends to the specific player if specified, or all players if nil
+--- Called from client: Player argument does nothing
+function XCF.SetServerData(Key, Value, TargetPlayer)
 	local DataVar = XCF.DataVars[Key]
-	local Player = Target or ServerString -- Player entity, or the server string
-	local ForServerData = Player == ServerString -- Are we trying to change the server's data or the client's?
-	if not ForServerData and not IsValid(Player) then error("XCF: Invalid player in SetDataVar") return end -- Invalid player, ignore
-	if DataVar.Values[Player] ~= Value then
-		DataVar.Values[Player] = Value
+	if DataVar.Values[ServerKey] ~= Value then
+		DataVar.Values[ServerKey] = Value
+		if SERVER then SendDataVar(DataVar, Value, true, TargetPlayer)
+		else SendDataVar(DataVar, Value, true) end
+	end
+end
 
-		net.Start("XCF_DV_NET")
-		net.WriteUInt(DataVar.UUID, XCF_DATA_VAR_LIMIT_EXPONENT) -- Encoded Key
-		net.WriteBool(ForServerData)
+--- Synchronizes client data with the other realm
+--- Called from server: Sends to the specific player if specified, or all players if nil
+--- Called from client: Player argument does nothing
+function XCF.SetClientData(Key, Value, TargetPlayer)
+	local DataVar = XCF.DataVars[Key]
 
-		DataVar.Type.Write(Value)
+	-- Called from client: use local player
+	-- Called from server: use player argument or all players if nil
+	local PlayersToSync = CLIENT and {LocalPlayer()} or (TargetPlayer and {TargetPlayer} or player.GetAll())
 
-		if SERVER then
-			-- Broadcast server change to all clients / Network client change to a single client
-			if ForServerData then
-				net.Broadcast()
-			else
-				if XCF.IsListenServer then net.Broadcast() -- Need to do this for loopback servers
-				else net.Send(Player) end
-			end
-		elseif CLIENT then
-			-- Network server / client change to server
-			net.SendToServer()
+	-- Iterate over the player(s) and update their values
+	for _, ply in ipairs(PlayersToSync) do
+		if DataVar.Values[ply] ~= Value then
+			DataVar.Values[ply] = Value
+			SendDataVar(DataVar, Value, false, ply)  -- Send update immediately after modification
 		end
 	end
 end
 
---- Receive data var updates (SECURITY IS VERY IMPORTANT, NEVER TRUST THE CLIENT)
+-- Handle incoming data var updates
 net.Receive("XCF_DV_NET", function(len, ply)
-	if len > (XCF_DATA_VAR_MAX_MESSAGE_SIZE * 8) then return end -- Someone is being evil and sending a huge packet
+	if len > (XCF_DATA_VAR_MAX_MESSAGE_SIZE * 8) then return end
 
-	local ID = net.ReadUInt(XCF_DATA_VAR_LIMIT_EXPONENT) -- Encoded Key (always a number)
-
+	local ID = net.ReadUInt(XCF_DATA_VAR_LIMIT_EXPONENT)
 	local Key = XCF.DataVarIDsToNames[ID]
-	if not Key then return end -- Invalid Key, ignore
+	if not Key then return end
 
 	local DataVar = XCF.DataVars[Key]
-	if not DataVar then return end -- Invalid DataVar, ignore
+	if not DataVar then return end
 
-	local ForServerData = net.ReadBool() -- Are we trying to change the server's data or the client's?
+	local SyncServerRealm = net.ReadBool()
+	local Value = DataVar.Type.Read()
 
-	local Player
-	if SERVER then Player = ForServerData and ServerString or ply -- Server receives have a ply argument
-	else Player = ForServerData and ServerString or LocalPlayer() end -- Client receives don't have a ply argument
+	-- Unauthorized clients cannot set the server's view of the data
+	if SERVER and SyncServerRealm and not XCF.CanSetServerData(ply) then return end
 
-	-- Only authorized users can set server's server data
-	if SERVER and ForServerData and not XCF.CanSetServerData(ply) then return end
-	DataVar.Values[Player] = DataVar.Type.Read()
-end)
-
--- Cleanup values when a player leaves to avoid stale data
-hook.Add("PlayerDisconnected", "XCF_CleanupDataVars", function(ply)
-	for _, dv in pairs(XCF.DataVars) do
-		dv.Values[ply] = nil
+	if SERVER then
+		if SyncServerRealm then DataVar.Values[ServerKey] = Value
+		else DataVar.Values[ply] = Value end
+	else
+		if SyncServerRealm then DataVar.Values[ServerKey] = Value
+		else DataVar.Values[LocalPlayer()] = Value end
 	end
 end)
+
+if SERVER then
+	-- Cleanup values when a player leaves to avoid stale data
+	hook.Add("PlayerDisconnected", "XCF_CleanupDataVars", function(ply)
+		for _, dv in pairs(XCF.DataVars) do
+			dv.Values[ply] = nil
+		end
+	end)
+
+	-- When a player loads in, send them all the server data so they have the correct values
+	hook.Add("XCF_OnLoadPlayer", "XCF_FullServerDataSync", function(ply)
+		if not IsValid(ply) then return end
+
+		for _, DataVar in pairs(XCF.DataVars) do
+			local value = DataVar.Values["Server"]
+			if value ~= nil then
+				SendDataVar(DataVar, value, true, ply)
+			end
+		end
+	end)
+end
 
 --- Load data vars from a file. Used for persistent data on client/server and presets on client
 -- function XCF.LoadDataVarsFromFile(Path, Filter, Blacklist) end
