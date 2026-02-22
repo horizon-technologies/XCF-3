bc2c535552d637834ac71b2fbecbdbdac706a148
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-02-22 06:22:46 -0500

    Document

--- lua/xcf/core/networking/data_vars/presets_sh.lua
+++ lua/xcf/core/networking/data_vars/presets_sh.lua
@@ -3,10 +3,10 @@ XCF.PresetsByGroupAndName = XCF.PresetsByGroupAndName or {} -- Maps Group -> Nam
 local BasePath = "xcf/presets/" -- Base path preset folders/files are located at
 
 --- Creates a preset with the given information
---- @param PresetName string
---- @param PresetGroup string
---- @param DataVarGroup string
---- @param SaveUnset boolean
+--- @param PresetName string The name of the preset. Must be unique within the PresetGroup.
+--- @param PresetGroup string The group of the preset. Presets are organized by group, and presets in the same group share the same set of data variables.
+--- @param DataVarGroup string The data variable group that this preset is associated with.
+--- @param SaveUnset boolean Whether to save unset data variables using their default. Not specifying allows you to only apply changes to what you want.
 function XCF.AddPreset(PresetName, PresetGroup, DataVarGroup, SaveUnset)
 	local NewPreset = {
 		Name = PresetName,
@@ -29,6 +29,7 @@ function XCF.AddPreset(PresetName, PresetGroup, DataVarGroup, SaveUnset)
 	return NewPreset
 end
 
+--- Removes a preset by name and group. Also deletes the preset file from disk if it exists.
 function XCF.RemovePreset(Name, Group)
 	if XCF.PresetsByGroupAndName[Group][Name] then
 		local Path = BasePath .. Name .. ".txt"
@@ -42,6 +43,7 @@ function XCF.RemovePreset(Name, Group)
 	return false
 end
 
+--- Sets all the data variables to values listed in the preset.
 function XCF.ApplyPreset(Name, Group)
 	local Preset = XCF.PresetsByGroupAndName[Group][Name]
 	if not Preset then return end
