94cf2efda88c0de48d96e38ee717b9214e7f21af
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-03-01 01:31:45 -0500

    Satisfy Linter

--- lua/xcf/core/classes/baseplates.lua
+++ lua/xcf/core/classes/baseplates.lua
@@ -1,13 +1,13 @@
 if SERVER then
-    local Baseplate = XCF.DefineClass("Baseplate", nil, function(Class, BaseClass)
+    XCF.DefineClass("Baseplate", nil, function(Class, BaseClass)
         print("Baseplate class initialized", Class, BaseClass)
     end)
 
-    local AircraftBaseplate = XCF.DefineClass("AircraftBP", "Baseplate", function(Class, BaseClass)
+    XCF.DefineClass("AircraftBP", "Baseplate", function(Class, BaseClass)
         print("AircraftBaseplate class initialized", Class, BaseClass)
     end)
 
-    local GroundVehicleBaseplate = XCF.DefineClass("GroundVehicleBP", "Baseplate", function(Class, BaseClass)
+    XCF.DefineClass("GroundVehicleBP", "Baseplate", function(Class, BaseClass)
         print("GroundVehicleBaseplate class initialized", Class, BaseClass)
     end)
 
